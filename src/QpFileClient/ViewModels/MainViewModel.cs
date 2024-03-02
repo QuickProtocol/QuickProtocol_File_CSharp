d9d74571ce9fa121f1ea9e17d4a18b42ac2d7ed8
张鹏 <scbeta@qq.com>
2024-03-02 16:48:20 +0800

    客户端界面设计中

--- src/QpFileClient/ViewModels/MainViewModel.cs
+++ src/QpFileClient/ViewModels/MainViewModel.cs
@@ -2,5 +2,22 @@
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public string ConnectionString { get; set; }
+    public string User { get; set; }
+    public string Password { get; set; }
+    public string LocalPath { get; set; }
+    public string RemotePath { get; set; }
+    public MainViewModel()
+    {
+#if DEBUG
+        ConnectionString = "qp.tcp://127.0.0.1:16421?Password=QpFile";
+        User = "test";
+        Password = "test";
+#endif
+    }
+
+    public void Connect()
+    {
+
+    }
 }
